0db4d2f5800582488b0128d41eac7efc3d6b09a5
Gavin Vickery <gavin@geekforbrains.com>
2025-07-03 07:25:20 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -33,6 +33,12 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+-- Disable unused providers for better performance
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
 vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
@@ -100,11 +106,77 @@ require("nvim-tree").setup({
     vim.keymap.set('n', 's', api.node.open.vertical, opts)
   end,
   sort = { sorter = "case_sensitive" },
-  view = { width = 24 },
-  renderer = { group_empty = true },
+  view = { 
+    width = 24,
+    preserve_window_proportions = true,  -- Prevent nvim-tree from resizing
+    adaptive_size = false,  -- Keep fixed width
+    debounce_delay = 15,  -- Default is good for performance
+  },
+  renderer = { 
+    group_empty = true,
+    icons = {
+      show = {
+        git = false,  -- Disable git icons for performance
+        diagnostics = false,  -- Disable diagnostic icons
+      },
+    },
+    highlight_git = "none",  -- Disable git highlighting
+    highlight_diagnostics = "none",  -- Disable diagnostics highlighting
+    highlight_opened_files = "none",  -- Disable opened files highlighting
+    highlight_modified = "none",  -- Disable modified highlighting
+  },
   filters = {
     dotfiles = false,  -- Show dotfiles (like .env)
-    custom = { "node_modules", "__pycache__", "^env$", "^.git$" },  -- Hide "env/" directory
+    custom = { 
+      "node_modules", 
+      "__pycache__", 
+      "^env$", 
+      "^.git$",
+      "dist",
+      "build",
+      ".next",
+      ".nuxt",
+      ".cache",
+      "coverage",
+      ".pytest_cache",
+      ".mypy_cache",
+      "*.egg-info",
+    },  -- Expanded filter list for better performance
+  },
+  git = {
+    enable = false,  -- Disable git integration for major performance improvement
+  },
+  diagnostics = {
+    enable = false,  -- Already disabled, keeping for clarity
+  },
+  modified = {
+    enable = false,  -- Disable modified status tracking
+  },
+  filesystem_watchers = {
+    enable = true,
+    debounce_delay = 50,
+    ignore_dirs = {
+      "/node_modules",
+      "/.git",
+      "/dist",
+      "/build",
+      "/.next",
+      "/__pycache__",
+    },
+  },
+  update_focused_file = {
+    enable = false,  -- Disable auto-update when changing files
+  },
+  actions = {
+    open_file = {
+      resize_window = false,  -- Prevent window resizing overhead
+      window_picker = {
+        enable = false,  -- Disable window picker to prevent resize issues
+      },
+    },
+    change_dir = {
+      enable = false,  -- Prevent directory changes from affecting window size
+    },
   },
 })
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
@@ -186,7 +258,10 @@ end
 
 -- Mason and LSP configuration
 require("mason").setup()
-require("mason-lspconfig").setup({ ensure_installed = { "pylsp" } })
+require("mason-lspconfig").setup({ 
+  ensure_installed = { "pylsp", "jsonls" },
+  automatic_enable = false  -- Disable automatic setup to prevent duplicates
+})
 
 -- Setup for Python LSP
 require("lspconfig").pylsp.setup({
@@ -223,6 +298,21 @@ vim.api.nvim_create_autocmd("VimEnter", {
       require("nvim-tree.api").tree.toggle({ focus = false })
     else
       require("nvim-tree.api").tree.open()
+    end
+  end,
+})
+
+-- Force nvim-tree to maintain its width when opening files after closing the last one
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  callback = function()
+    -- Only run if we just opened a file and nvim-tree exists
+    local tree_win = vim.fn.win_findbuf(vim.fn.bufnr("NvimTree_1"))[1]
+    if tree_win and #vim.api.nvim_list_wins() == 2 then
+      vim.defer_fn(function()
+        if vim.api.nvim_win_is_valid(tree_win) then
+          vim.api.nvim_win_set_width(tree_win, 24)
+        end
+      end, 10)
     end
   end,
 })
